741e83d4be2e46dcca558f0b92e5de7f27b2818f
lanalebedeva <batetnikova@mail.ru>
2023-02-09 15:07:50 +0300

    add: The second activity displays the square of the number from the first activate, but when changing the configuration, the number does not increase

--- app/src/main/java/com/github/lanalebedeva/task02activity/SecondActivity.kt
+++ app/src/main/java/com/github/lanalebedeva/task02activity/SecondActivity.kt
@@ -1,44 +1,30 @@
 package com.github.lanalebedeva.task02activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var numberView: TextView
-    private var number = 0
-
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        numberView = findViewById<TextView>(R.id.tv_mainActivity_number)
-        if (savedInstanceState != null) {
-            number = savedInstanceState.getInt(NUMBER)
-            ++number
-        }
+        setContentView(R.layout.activity_second)
+
+        val bundle = intent.extras
+        val number = bundle?.getInt(MainActivity.NUMBER) ?: 0
+        val textView = findViewById<TextView>(R.id.tv_secondActivity_number)
+        textView.text = (number * number).toString()
         val nameFunThread = Thread.currentThread().stackTrace[2].methodName
-        numberView.text = number.toString()
         Log.d(
             TAG,
             "callback: $nameFunThread; lifecycle.currentState.name: ${lifecycle.currentState.name}"
         )
-
-        val buttonToSecondActivity = findViewById<Button>(R.id.btn_goToSecondActivity)
-        buttonToSecondActivity.setOnClickListener{
-            val intentToSecondActivity = Intent(this, SecondActivity::class.java).apply {
-                putExtra(NUMBER, number)
-            }
-            startActivity(intentToSecondActivity)
-        }
     }
 
     override fun onStart() {
         val nameFunThread = Thread.currentThread().stackTrace[2].methodName
         Log.d(
-            TAG,
+            MainActivity.TAG,
             "callback: $nameFunThread; lifecycle.currentState.name: ${lifecycle.currentState.name}"
         )
         super.onStart()
@@ -79,19 +65,7 @@ class MainActivity : AppCompatActivity() {
         )
         super.onDestroy()
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        val nameFunThread = Thread.currentThread().stackTrace[2].methodName
-        Log.d(
-            TAG,
-            " callback: $nameFunThread; lifecycle.currentState.name: ${lifecycle.currentState.name}"
-        )
-        outState.putInt(NUMBER, number)
-        super.onSaveInstanceState(outState)
-    }
-
     companion object {
-        const val NUMBER = "number"
-        const val TAG = "mainActivity"
+        const val TAG = "SecondActivity"
     }
 }
